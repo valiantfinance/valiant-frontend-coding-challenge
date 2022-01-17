037c11728bbe7c0752ade6c906a282673d0548d4
Luke Thompson <luke.thompson@valiant.finance>
2022-01-17 14:54:15 +1100

    :wrench: Remove data key from endpoints

--- backend/index.js
+++ backend/index.js
@@ -5,79 +5,73 @@ const app = express()
 app.use(cors())
 
 app.get('/loan-purposes', (req, res) => {
-  res.json({
-    data: [
-      {
-        label: 'Day-to-day capital',
-        value: 'general',
-        annualRate: 0.1,
-      },
-      {
-        label: 'Vehicle or transport',
-        value: 'vehicle',
-        annualRate: 0.045,
-      },
-      {
-        label: 'Financing a property',
-        value: 'property',
-        annualRate: 0.029,
-      },
-    ],
-  })
+  res.json([
+    {
+      label: 'Day-to-day capital',
+      value: 'general',
+      annualRate: 0.1,
+    },
+    {
+      label: 'Vehicle or transport',
+      value: 'vehicle',
+      annualRate: 0.045,
+    },
+    {
+      label: 'Financing a property',
+      value: 'property',
+      annualRate: 0.029,
+    },
+  ])
 })
 
 app.get('/requested-repayment-periods', (req, res) => {
-  res.json({
-    data: [
-      {
-        label: 'Weekly',
-        value: 52,
-      },
-      {
-        label: 'Fortnightly',
-        value: 26,
-      },
-      {
-        label: 'Monthly',
-        value: 12,
-      },
-    ],
-  })
+  res.json([
+    {
+      label: 'Weekly',
+      value: 52,
+    },
+    {
+      label: 'Fortnightly',
+      value: 26,
+    },
+    {
+      label: 'Monthly',
+      value: 12,
+    },
+  ])
 })
 
 app.get('/requested-term-months', (req, res) => {
-  res.json({
-    data: [
-      {
-        label: '6 months',
-        value: 6,
-      },
-      {
-        label: '12 months',
-        value: 12,
-      },
-      {
-        label: '2 years',
-        value: 24,
-      },
-      {
-        label: '3 years',
-        value: 36,
-      },
-      {
-        label: '5 years',
-        value: 60,
-      },
-      {
-        label: '10 years',
-        value: 120,
-      },
-      {
-        label: '20 years',
-        value: 240,
-      },
-    ],
-  })
+  res.json([
+    {
+      label: '6 months',
+      value: 6,
+    },
+    {
+      label: '12 months',
+      value: 12,
+    },
+    {
+      label: '2 years',
+      value: 24,
+    },
+    {
+      label: '3 years',
+      value: 36,
+    },
+    {
+      label: '5 years',
+      value: 60,
+    },
+    {
+      label: '10 years',
+      value: 120,
+    },
+    {
+      label: '20 years',
+      value: 240,
+    },
+  ])
 })
 
 app.listen(5000, () => {
