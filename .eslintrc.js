e6f11ffb5a44fcb10bef8847a4d860da66091878
Luke Thompson <luke.thompson@valiant.finance>
2022-01-17 11:03:19 +1100

    :balloon: Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,35 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/recommended',
+    '@vue/standard',
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Enforce style of a single space before curly bracket open & close.
+    'object-curly-spacing': ['error', 'always'],
+    // Enforce style of no spaces before array open & close.
+    'array-bracket-spacing': ['error', 'never'],
+    // Require comma-dangle in multi-line arrays and objects.
+    'comma-dangle': ['error', 'always-multiline'],
+
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 }
