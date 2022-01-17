63fc36d5746f7bae19e4817c0d7406c11a6999ab
Luke Thompson <luke.thompson@valiant.finance>
2022-01-17 14:44:37 +1100

    :wrench: Mock an API using express

---
+++ backend/index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/loan-purposes', (req, res) => {
+  res.json({
+    data: [
+      {
+        label: 'Day-to-day capital',
+        value: 'general',
+        annualRate: 0.1,
+      },
+      {
+        label: 'Vehicle or transport',
+        value: 'vehicle',
+        annualRate: 0.045,
+      },
+      {
+        label: 'Financing a property',
+        value: 'property',
+        annualRate: 0.029,
+      },
+    ],
+  })
+})
+
+app.get('/requested-repayment-periods', (req, res) => {
+  res.json({
+    data: [
+      {
+        label: 'Weekly',
+        value: 52,
+      },
+      {
+        label: 'Fortnightly',
+        value: 26,
+      },
+      {
+        label: 'Monthly',
+        value: 12,
+      },
+    ],
+  })
+})
+
+app.get('/requested-term-months', (req, res) => {
+  res.json({
+    data: [
+      {
+        label: '6 months',
+        value: 6,
+      },
+      {
+        label: '12 months',
+        value: 12,
+      },
+      {
+        label: '2 years',
+        value: 24,
+      },
+      {
+        label: '3 years',
+        value: 36,
+      },
+      {
+        label: '5 years',
+        value: 60,
+      },
+      {
+        label: '10 years',
+        value: 120,
+      },
+      {
+        label: '20 years',
+        value: 240,
+      },
+    ],
+  })
+})
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000')
+})
