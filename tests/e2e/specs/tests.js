7c9c08e7e698e423c5b9c27d6dbf38f6d159bc88
Luke Thompson <luke.thompson@valiant.finance>
2024-03-11 16:46:25 +1100

    :white_check_mark: Fix url on example test

--- tests/e2e/specs/tests.js
+++ tests/e2e/specs/tests.js
@@ -1,6 +1,6 @@
 describe('Example test', () => {
   it('runs the app', () => {
-    cy.visit('http://127.0.0.1:5173/')
-    cy.url().should('eq', 'http://127.0.0.1:5173/')
+    cy.visit('http://localhost:5173/')
+    cy.url().should('eq', 'http://localhost:5173/')
   })
 })
